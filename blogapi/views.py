83114c643ad26ffad5d8aedc86f552f698509c1b
osadolortech <brightosadolor8@gmail.com>
2022-06-20 20:40:31 +0100

    updateing the settings pagegination

--- blogapi/views.py
+++ blogapi/views.py
@@ -1,9 +1,12 @@
 from rest_framework import viewsets
 from .models import BlogpotslList
 from .serializers import Blogserilizers
+from rest_framework.pagination import LimitOffsetPagination
 
 # Create your views here.
 
 class BlogApi(viewsets.ModelViewSet):
     queryset = BlogpotslList.objects.all()
     serializer_class = Blogserilizers
+    pagination_class = LimitOffsetPagination
+
